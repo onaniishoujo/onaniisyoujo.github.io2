45964be23efc34133bdb5908c04fa9553d1e927f
onaniisyoujo <crazycute@163.com>
2023-10-14 17:28:28 +0800

    网格背景

--- js/linkcss.js
+++ js/linkcss.js
@@ -5,8 +5,8 @@ document.addEventListener('DOMContentLoaded', function() {
     // 随机生成0到9之间的整数
     var randomIndex = Math.floor(Math.random() * 10);
 
-    // 使用绝对路径构建图片文件名，例如：http://example.com/img/bodyX.jpg
-    var imageName = 'https://onaniisyoujo.github.io/img/body' + randomIndex + '.jpg';
+    // 构建相对路径的图片文件名
+    var imageName = '/img/body' + randomIndex + '.jpg';
 
     // 直接设置body元素的背景图片
     document.body.style.backgroundImage = 'url(' + imageName + ')';
