8f585f0649aa3f3e3aaee329686eb324c74aa085
onaniisyoujo <crazycute@163.com>
2023-10-14 22:29:44 +0800

    文章 20231014
    为Hexo-Fluid的文章设置网格背景
    修改Hexo-Fluid文章中代码的字体及背景色
    谁能帮我找个女朋友

--- js/back.js
+++ js/back.js
@@ -1,11 +1,8 @@
-window.onload = function() {
-  // 随机生成0到9之间的整数
-  var randomIndex = Math.floor(Math.random() * 10);
+var randomIndex = Math.floor(Math.random() * 10);
 
-  // 构建相对路径的图片文件名
-  var imageName = '/img/body' + randomIndex + '.jpg';
+// 构建相对路径的图片文件名
+var imageName = '/img/body' + randomIndex + '.jpg';
 
-  // 直接设置body元素的背景图片和背景大小
-  document.body.style.backgroundImage = 'url(' + imageName + ')';
-  document.body.style.backgroundSize = 'cover';
-};
+// 直接设置body元素的背景图片和背景大小
+document.body.style.backgroundImage = 'url(' + imageName + ')';
+document.body.style.backgroundSize = 'cover';
