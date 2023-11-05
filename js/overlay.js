295e177b20c594d3007b503043286c50b5e17909
onaniisyoujo <crazycute@163.com>
2023-11-05 19:11:01 +0800

    文章，背景源，蒙版
    少女的人设
    蒙版3s改为2s

--- js/overlay.js
+++ js/overlay.js
@@ -1,14 +1,19 @@
 var overlay = document.getElementById('overlay');
 var loading = document.getElementById('loading');
 var wave = document.getElementById('wave');
+var myImage = document.getElementById('image');
 
 var timer = setTimeout(function() {
   hideOverlay();
-}, 3000);
+}, 2000); // 将加载时间改为2秒
 
 window.addEventListener('load', function() {
   clearTimeout(timer); // 在页面加载完成后清除定时器
   hideOverlay();
+});
+
+myImage.addEventListener('error', function() {
+  hideOverlay(); // 当图片加载失败时，手动触发隐藏蒙版的函数
 });
 
 function hideOverlay() {
