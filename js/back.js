0556ae3ff0885d87a7ab083c816ded3e9e2a735e
onaniisyoujo <crazycute@163.com>
2023-12-17 22:00:48 +0800

    valine 修复头像bug
    将valine的cdn改为本地

--- js/back.js
+++
@@ -1,8 +0,0 @@
-var randomIndex = Math.floor(Math.random() * 10);
-
-// 构建相对路径的图片文件名
-var imageName = '/img/body' + randomIndex + '.jpg';
-
-// 直接设置body元素的背景图片和背景大小
-document.body.style.backgroundImage = 'url(' + imageName + ')';
-document.body.style.backgroundSize = 'cover';
